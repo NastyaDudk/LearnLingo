15d1a7ee769a964990517acd12c0df2d6c7de51b
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-10-01 12:39:04 +0400

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const DATABASE_URI = import.meta.env.VITE_DATABASE_URI;
+const DATABASE_URL = import.meta.env.VITE_DATABASE_URL;
 const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
@@ -13,7 +13,7 @@ const APP_ID = import.meta.env.VITE_APP_ID;
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URI,
+  databaseURL: DATABASE_URL,
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
