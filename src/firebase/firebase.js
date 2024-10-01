73c62db4ebabda6f9b9f88b3f86bce0a5dfcd7c9
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-10-01 13:39:14 +0400

    ok

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,8 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-const API_KEY = "AIzaSyAXOHSnwXDXyo08kuoU4yEaQGLMAoHlhNo";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const DATABASE_URL = import.meta.env.VITE_DATABASE_URL;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
 const APP_ID = import.meta.env.VITE_APP_ID;
@@ -11,9 +13,8 @@ const APP_ID = import.meta.env.VITE_APP_ID;
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
-  databaseURL:
-    "https://learnlingo-7d407-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "learnlingo-7d407",
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
